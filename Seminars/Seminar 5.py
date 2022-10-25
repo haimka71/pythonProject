bd95d9e11b5dc631e8b2e7e1bf86c36e237b73b2
Anatoly Berezhnoy <109862436+haimka71@users.noreply.github.com>
2022-10-25 16:06:31 +0300

    Seminars 5 Task 3 RLE

--- Seminars/Seminar 5.py
+++ Seminars/Seminar 5.py
@@ -2,6 +2,9 @@
 # Напишите программу, удаляющую из текста все слова, содержащие "абв".
 # Пример:
 # Входные данные: 'ываабвавб лепотабв это абвцукен абвабвгкруто ываы круто'
+from pydoc import text
+
+import txt as txt
 
 
 # txt = input("Введите текст через пробел:\n")
@@ -141,6 +144,116 @@
 # Источник: https://tonais.ru/osnovy/kodirovanie-dlin-seriy-rle-python
 
 
+
+def coding(txt):  # функция кодирования
+    count = 1
+    res = ''
+    for i in range(len(txt) - 1):
+        if txt[i] == txt[i + 1]:
+            count += 1
+        else:
+            res = res + str(count) + txt[i]
+            count = 1
+    if count > 1 or (txt[len(txt) - 2] != txt[-1]):
+        res = res + str(count) + txt[-1]
+    return res
+
+def decoding(txt):     # функция декодирования вариант 1
+    number = ''
+    res = ''
+    for i in range(len(txt)):
+        if not txt[i].isalpha():
+            number += txt[i]
+        else:
+            res = res + txt[i] * int(number)
+            number = ''
+    return res
+
+
+
+# def decoding(txt):  # функция декодирования вариант 2
+#  number= ''
+#  res = ''
+#  for i in range(len(txt)):
+#         if not txt[i].isalpha(): # The isalpha() method returns True if all the characters are alphabet letters (a-z)
+#             number += txt[i]
+#         else:
+#             res += (txt[i] * int(number))
+#             # res.append(txt[i] * int(number))
+#             number = ''
+#             # number += i
+#  return ''.join(res)
+
+
+
+# def decoding(file):           # функция декодирования вариант 3 (семинар)
+#   txt = open(file).read()
+#   lenth = 0
+#   wt_txt = ''
+#   while lenth < len(txt):
+#      amount = ''
+#      key = ''
+#      while txt[lenth].isdigit():
+#        amount += txt[lenth]
+#        lenth +=1
+#        key += txt[lenth]
+#        wt_txt += int(amount)*key
+#        lenth +=1
+#   with open('file111.txt','w+') as rd:
+#        text.write(f'{wt_txt}')
+
+# decoding('file111.txt')
+
+
+s = input("Введите текст для кодировки (без пробелов и разделителей): ")  # с вводом вручную
+# s= open('file.txt').read()                                                  #  чтение из file.txt
+print(f"Текст до кодировки: {s}")
+
+print(f"Текст после кодировки: {coding(s)}")
+with open('file111.txt', 'w+') as rd:                                       # записали в file111.txt
+    rd.write(coding(s))
+
+# print(f'Текст после декодировки: {decoding(coding(s))}')
+
+sum = open('file111.txt').read()
+print(f'Текст после дешифровки: {decoding(sum)}')
+with open('file 222.txt', 'w+') as rd:                             # записали результат дешифровки в file222.txt
+    rd.write(decoding(sum))
+
+
+
+
+
+
+
+
+
+# print(f"Текст после дешифровки: {decoding(coding(s))}")
+# with open('file111.txt','r') as rd:
+#  s1=list(map(int,rd.read().split(' ')))
+# print(f"Текст после дешифровки: {decoding(coding(s1))}")
+# print(f"Текст после кодировки: {coding(s)}")
+
+
+
+
+
+
+
+
+
+# def decode(txt):
+#  result=[]
+#  for item in txt:
+#     result.append(item[0]*item[1])
+#     return " ".join(result)
+
+# with open('file111.txt','r') as rd:
+#  sum=list(map(str,rd.read().split(' ')))
+#  # sum = open('file111.txt').read()
+# print(f"Текст после дешифровки: {decoding(sum)}")
+
+############# Вариант с char, надо разбираться
 # def rle_encode(data): encoding = '' prev_char = '' count = 1
 # if not data: return '' for char in data:
 # #	If the prev and current characters
@@ -157,38 +270,3 @@
 # 	# Finish off the encoding
 # 	encoding += str(count) + prev_char ret encoding
 ######################
-
-def coding(txt):  # функция кодирования
-    count = 1
-    res = ''
-    for i in range(len(txt) - 1):
-        if txt[i] == txt[i + 1]:
-            count += 1
-        else:
-            res = res + str(count) + txt[i]
-            count = 1
-    if count > 1 or (txt[len(txt) - 2] != txt[-1]):
-        res = res + str(count) + txt[-1]
-    return res
-
-
-def decoding(txt):  # функция декодирования
-    number = ''
-    res = ''
-    for i in range(len(txt)):
-        if not txt[i].isalpha():
-            number += txt[i]
-        else:
-            res = res + txt[i] * int(number)
-            number = ''
-    return res
-
-
-# s = input("Введите текст для кодировки (без пробелов и разделителей): ")  # с вводом вручную
-s= open('file.txt').read()                                                  #  чтение из file.txt
-print(f"Текст до кодировки: {s}")
-print(f"Текст после кодировки: {coding(s)}")
-with open('file111.txt', 'w+') as rd:                                       # записали в file111.txt
-    rd.write(coding(s))
-
-

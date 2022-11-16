ecd3be3acd042a0e6ea61f6fb339ea576cfd2cb9
«Anatoly <haimka71@mail.ru>
2022-11-16 15:39:51 +0300

    Game X-0 with Bot created Seminar 10 commit

--- BOT Project/Bot Task.py
+++ BOT Project/Bot Task.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher, types
 logging.basicConfig(level=logging.INFO)
 # dp = Dispatcher(bot)
 
-bot = Bot(token = "5456204040:AAGNo61KJdwsJejgIBx7K_qtItrf41L6CQY")
+bot = Bot(token = "545___:AAGNo61KJ___6CQY")
 dp = Dispatcher(bot)
 @dp.message_handler(commands =['start'])
 async def cmd_start (message: types.Message):
